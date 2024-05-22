676a3cfce184edd38e60b546b5711b000c5ce485
MuhammadFahdel10 <162016990+MuhammadFahdel10@users.noreply.github.com>
2024-05-22 23:27:31 +0700

    Add files via upload

---
+++ Web development 2.1 (Muhammad Fahdel Adha Risdiyanto).jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hi, my name is - Muhammad Fahdel!</h1>
+        <p>Currently, I am 18 years old and I live in Jatinangor City.</p>
+        <p>My full name is Muhammad Fahdel Adha Risdiyanto and I am a freshman at Padjadjaran University.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
